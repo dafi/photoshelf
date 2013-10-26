703787bbabd265ec55747903dac61ec3044cf3ab
Davide Ficano <davide.ficano@gmail.com>
2013-10-26 17:04:32 +0200

    Added 'delete post'

--- app/src/com/ternaryop/tumblr/Tumblr.java
+++ app/src/com/ternaryop/tumblr/Tumblr.java
@@ -250,6 +250,36 @@ public class Tumblr {
         }.execute();
     }
 
+    public void deletePost(final String tumblrName, final long id, final Callback<JSONObject> callback) {
+        new AsyncTask<Void, Void, JSONObject>() {
+        	Exception error;
+    		@Override
+    		protected JSONObject doInBackground(Void... params) {
+    	        String apiUrl = getApiUrl(tumblrName, "/post/delete");
+    	    	
+    	        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+    	        nameValuePairs.add(new BasicNameValuePair("id", id + ""));
+
+    	        try {
+        	        JSONObject json = consumer.jsonFromPost(apiUrl, nameValuePairs);
+        	        return json.getJSONObject("response");
+				} catch (Exception e) {
+					error = e;
+				}
+    	        return null;
+    		}
+
+			@Override
+			protected void onPostExecute(JSONObject response) {
+				if (error == null) {
+					callback.complete(instance, response);
+				} else {
+					callback.failure(instance, error);
+				}
+			}
+        }.execute();
+    }
+
     public void saveDraft(final String tumblrName, final long id, final Callback<JSONObject> callback) {
         new AsyncTask<Void, Void, JSONObject>() {
         	Exception error;
