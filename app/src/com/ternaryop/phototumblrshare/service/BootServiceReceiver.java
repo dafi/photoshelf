95fd2f03c49febfff45477ed7f62d08b78741545
Davide Ficano <davide.ficano@gmail.com>
2013-11-17 17:18:01 +0100

    Delay birthday check and use an alarm to repeat

--- app/src/com/ternaryop/phototumblrshare/service/BootServiceReceiver.java
+++ app/src/com/ternaryop/phototumblrshare/service/BootServiceReceiver.java
@@ -1,4 +1,9 @@
 package com.ternaryop.phototumblrshare.service;
+
+import java.util.Calendar;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,11 +13,24 @@ import android.content.Intent;
 
 public class BootServiceReceiver extends BroadcastReceiver {
 	@Override
-	public void onReceive(Context context, Intent intent) {
+	public void onReceive(final Context context, final Intent intent) {
 		if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-			Intent serviceIntent = new Intent(context.getApplicationContext(), BootService.class);
-			serviceIntent.setAction(BootService.BIRTHDAY_ACTION);
-			context.startService(serviceIntent);
+			// don't slow the boot running immediately the service, wait 5 seconds
+			createAlarm(context, Calendar.getInstance().getTimeInMillis() + 5000);
 		}
 	}
+
+	private final void createAlarm(Context context, long triggerAtMillis) {
+		final Intent serviceIntent = new Intent(context.getApplicationContext(), BootService.class);
+		serviceIntent.setAction(BootService.BIRTHDAY_ACTION);
+
+        PendingIntent pendingIntent = PendingIntent.getService(context.getApplicationContext(),
+        		0,
+        		serviceIntent,
+        		PendingIntent.FLAG_CANCEL_CURRENT);
+
+        AlarmManager alarmManager = (AlarmManager)context.getApplicationContext()
+                .getSystemService(Context.ALARM_SERVICE);
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, triggerAtMillis, AlarmManager.INTERVAL_DAY, pendingIntent);
+    }
 }
