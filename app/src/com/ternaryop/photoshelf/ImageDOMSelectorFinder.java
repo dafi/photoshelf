db9ff03aa67c435d96bb463e40709cf461cae9fb
Davide Ficano <davide.ficano@gmail.com>
2014-08-10 07:57:35 +0200

    Added a version field to selector json file so it's possible to determine if an imported config is older than the one contained into application update

--- app/src/com/ternaryop/photoshelf/ImageDOMSelectorFinder.java
+++ app/src/com/ternaryop/photoshelf/ImageDOMSelectorFinder.java
@@ -1,17 +1,15 @@
 package com.ternaryop.photoshelf;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.regex.Pattern;
-
-import org.json.JSONObject;
 
 import android.content.Context;
 
 import com.ternaryop.utils.JSONUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Obtain the DOM selector used to search the image contained inside a given url
@@ -20,40 +18,58 @@ import com.ternaryop.utils.JSONUtils;
  * 
  */
 public class ImageDOMSelectorFinder {
-	private static HashMap<String, Object> domainMap = new HashMap<String, Object>();
+    private static HashMap<String, Object> domainMap = new HashMap<String, Object>();
+    private static boolean isUpgraded;
+    private static final String SELECTORS_FILENAME = "domSelectors.json";
 
-	public ImageDOMSelectorFinder(Context context) {
-		try {
-			synchronized (domainMap) {
-				InputStream is;
-				try {
-					is = context.openFileInput("domSelectors.json");
-				} catch (FileNotFoundException ex) {
-					is = context.getAssets().open("domSelectors.json");
-				}
-				BufferedReader bis = new BufferedReader(new InputStreamReader(is));
-				StringBuilder builder = new StringBuilder();
-				String line;
-				while ((line = bis.readLine()) != null) {
-					builder.append(line);
-				}
-				if (domainMap.isEmpty()) {
-					domainMap.putAll(JSONUtils.toMap(new JSONObject(builder.toString())));
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public ImageDOMSelectorFinder(Context context) {
+        try {
+            synchronized (domainMap) {
+                if (!domainMap.isEmpty()) {
+                    return;
+                }
+                InputStream is;
+                try {
+                    is = context.openFileInput(SELECTORS_FILENAME);
+                    // if an imported file exists and its version is minor than the file in assets we delete it
+                    if (!isUpgraded) {
+                        isUpgraded = true;
+                        JSONObject jsonPrivate = JSONUtils.jsonFromInputStream(is);
+                        JSONObject jsonAssets = JSONUtils.jsonFromInputStream(context.getAssets().open(SELECTORS_FILENAME));
+                        int privateVersion = -1;
 
-	public String getSelectorFromUrl(String url) {
-		if (url != null) {
-			for (String domainRE : domainMap.keySet()) {
-				if (Pattern.compile(domainRE).matcher(url).find()) {
-					return (String) domainMap.get(domainRE);
-				}
-			}
-		}
-		return null;
-	}
+                        // version may be not present on existing json file
+                        try {
+                            privateVersion = jsonPrivate.getInt("version");
+                        } catch (JSONException ex) {
+                        }
+                        int assetsVersion = jsonAssets.getInt("version");
+                        if (privateVersion < assetsVersion) {
+                            is.close();
+                            context.deleteFile(SELECTORS_FILENAME);
+                        }
+                        domainMap.putAll(JSONUtils.toMap(jsonAssets.getJSONObject("selectors")));
+                        return;
+                    }
+                } catch (FileNotFoundException ex) {
+                    is = context.getAssets().open(SELECTORS_FILENAME);
+                }
+                JSONObject jsonAssets = JSONUtils.jsonFromInputStream(is);
+                domainMap.putAll(JSONUtils.toMap(jsonAssets.getJSONObject("selectors")));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getSelectorFromUrl(String url) {
+        if (url != null) {
+            for (String domainRE : domainMap.keySet()) {
+                if (Pattern.compile(domainRE).matcher(url).find()) {
+                    return (String) domainMap.get(domainRE);
+                }
+            }
+        }
+        return null;
+    }
 }
