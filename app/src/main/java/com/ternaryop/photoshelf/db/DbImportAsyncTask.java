ba02e2fb9714bfe704de94220bab3e9a3a771584
Davide Ficano <davide.ficano@gmail.com>
2014-09-14 13:47:29 +0200

    Now extends AbsProgressIndicatorAsyncTask

--- app/src/main/java/com/ternaryop/photoshelf/db/DbImportAsyncTask.java
+++ app/src/main/java/com/ternaryop/photoshelf/db/DbImportAsyncTask.java
@@ -2,48 +2,32 @@ package com.ternaryop.photoshelf.db;
 
 import java.util.Iterator;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.os.AsyncTask;
+import android.widget.TextView;
 
 import com.ternaryop.photoshelf.R;
-import com.ternaryop.utils.DialogUtils;
+import com.ternaryop.utils.AbsProgressIndicatorAsyncTask;
 
-public class DbImportAsyncTask<Pojo> extends AsyncTask<Void, Integer, Void> {
-    private Exception error;
-    private ProgressDialog progressDialog;
-    private final Context context;
+public class DbImportAsyncTask<Pojo> extends AbsProgressIndicatorAsyncTask<Void, Integer, Void> {
     private final Iterator<Pojo> iterator;
     private final boolean removeAll;
     private final AbsDAO<Pojo> dao;
-    
-    public DbImportAsyncTask(Context context, Iterator<Pojo> iterator, AbsDAO<Pojo> dao, boolean removeAll) {
-        this.context = context;
+
+    public DbImportAsyncTask(Context context, TextView textView, Iterator<Pojo> iterator, AbsDAO<Pojo> dao, boolean removeAll) {
+        super(context, context.getString(R.string.start_import_title), textView);
         this.iterator = iterator;
         this.dao = dao;
         this.removeAll = removeAll;
     }
 
-    protected void onPreExecute() {
-        progressDialog = new ProgressDialog(context);
-        progressDialog.setMessage(context.getString(R.string.start_import_title));
-        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        progressDialog.show();
+    public DbImportAsyncTask(Context context, java.util.Iterator <Pojo> iterator, AbsDAO<Pojo> dao, boolean removeAll) {
+        this(context, null, iterator, dao, removeAll);
     }
-    
+
     @Override
     protected void onProgressUpdate(Integer... values) {
-        progressDialog.setMessage(context.getString(R.string.import_progress_title, values[0]));
-    }
-    
-    @Override
-    protected void onPostExecute(Void result) {
-        progressDialog.dismiss();
-        
-        if (error != null) {
-            DialogUtils.showErrorDialog(context, error);
-        }
+        setProgressMessage(getContext().getString(R.string.import_progress_title, values[0]));
     }
 
     @Override
@@ -63,7 +47,7 @@ public class DbImportAsyncTask<Pojo> extends AsyncTask<Void, Integer, Void> {
             db.setTransactionSuccessful();
         } catch (Exception e) {
             e.printStackTrace();
-            error = e;
+            setError(e);
         } finally {
             db.endTransaction();
         }
