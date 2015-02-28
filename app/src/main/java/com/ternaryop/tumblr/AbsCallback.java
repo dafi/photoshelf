fb0a5a6e5f4222b9eed300cb2aae89f08e7fec9c
Davide Ficano <davide.ficano@gmail.com>
2015-02-28 12:17:11 +0100

    ctor accepts DialogFragment

--- app/src/main/java/com/ternaryop/tumblr/AbsCallback.java
+++ app/src/main/java/com/ternaryop/tumblr/AbsCallback.java
@@ -1,6 +1,7 @@
 package com.ternaryop.tumblr;
 
 import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
 
 import com.ternaryop.utils.DialogUtils;
@@ -11,6 +12,7 @@ public abstract class AbsCallback implements Callback<JSONObject> {
     private final Context context;
     private final int errorResId;
     private Dialog dialog;
+    private DialogFragment dialogFragment;
 
     public AbsCallback(Context context, int errorResId) {
         this.context = context;
@@ -18,15 +20,28 @@ public abstract class AbsCallback implements Callback<JSONObject> {
     }
 
     public AbsCallback(Dialog dialog, int errorResId) {
+        this(dialog.getContext(), errorResId);
+        if (this.dialogFragment != null) {
+            throw new IllegalArgumentException("dialogFragment must be null if dialog is set");
+        }
         this.dialog = dialog;
-        this.context = dialog.getContext();
-        this.errorResId = errorResId;
     }
-    
+
+    public AbsCallback(DialogFragment dialog, int errorResId) {
+        this(dialog.getActivity(), errorResId);
+        if (this.dialog != null) {
+            throw new IllegalArgumentException("dialog must be null if dialogFragment is set");
+        }
+        this.dialogFragment = dialog;
+    }
+
     @Override
     public void failure(Exception e) {
         if (dialog != null) {
             dialog.dismiss();
+        }
+        if (dialogFragment != null) {
+            dialogFragment.dismiss();
         }
         DialogUtils.showErrorDialog(context, errorResId, e);
     }
