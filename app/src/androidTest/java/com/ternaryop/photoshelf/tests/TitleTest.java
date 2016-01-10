9c3a25807a43895e8d09e92ef170372625e7a38d
Davide Ficano <davide.ficano@gmail.com>
2016-01-10 11:33:10 +0100

    Simple title test

--- app/src/androidTest/java/com/ternaryop/photoshelf/tests/TitleTest.java
+++ app/src/androidTest/java/com/ternaryop/photoshelf/tests/TitleTest.java
@@ -1,7 +1,31 @@
 package com.ternaryop.photoshelf.tests;
 
+import android.test.AndroidTestCase;
+
+import com.ternaryop.photoshelf.parsers.AndroidTitleParserConfig;
+import com.ternaryop.photoshelf.parsers.TitleData;
+import com.ternaryop.photoshelf.parsers.TitleParser;
+
 /**
- * Created by dave on 10/01/16.
+ * Created by dave on 21/09/14.
+ * Ready to run test to used inside the emulator
  */
-public class TitleTest {
+public class TitleTest extends AndroidTestCase {
+    public void testTitle() {
+        // fill the array with titles
+        String arr[] = new String[]{
+                ""
+        };
+        try {
+            AndroidTitleParserConfig config = new AndroidTitleParserConfig(getContext());
+            for (String i : arr) {
+                TitleData titleData = TitleParser.instance(config).parseTitle(i);
+
+                String formattedInput = titleData.format("<strong>", "</strong>", "<em>", "</em>");
+                System.out.println("testTitle " + formattedInput);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
