90070e7923704b029d46c7eff91fac5596b86da5
Davide Ficano <davide.ficano@gmail.com>
2016-07-15 08:07:22 +0200

    Style works better on android 5.x No longer necessary to pass the theme to AlertDialog.Builder

--- app/src/main/java/com/ternaryop/photoshelf/fragment/AbsPostsListFragment.java
+++ app/src/main/java/com/ternaryop/photoshelf/fragment/AbsPostsListFragment.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
