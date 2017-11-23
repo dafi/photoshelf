1f1f710fa98b752eedbcfcec42bd43211ee1215c
Davide Ficano <davide.ficano@gmail.com>
2017-11-23 15:37:59 +0100

    Show the relative date as button label

--- app/src/main/java/com/ternaryop/photoshelf/dialogs/SchedulePostDialog.java
+++ app/src/main/java/com/ternaryop/photoshelf/dialogs/SchedulePostDialog.java
@@ -9,7 +9,7 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
-import android.content.DialogInterface;
+import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -20,7 +20,6 @@ import com.ternaryop.photoshelf.adapter.PhotoShelfPost;
 import com.ternaryop.tumblr.Tumblr;
 import io.reactivex.Completable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.Action;
 import io.reactivex.schedulers.Schedulers;
 
 public class SchedulePostDialog extends Dialog implements View.OnClickListener, TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener {
@@ -29,7 +28,6 @@ public class SchedulePostDialog extends Dialog implements View.OnClickListener, 
     private final Button chooseDateButton;
     private final Button chooseTimeButton;
     private final SimpleDateFormat timeFormat;
-    private final SimpleDateFormat dateFormat;
     private final PhotoShelfPost item;
     private final String blogName;
     private final onPostScheduleListener onPostSchedule;
@@ -48,14 +46,13 @@ public class SchedulePostDialog extends Dialog implements View.OnClickListener, 
 
         // init date
         chooseDateButton = (Button)findViewById(R.id.choose_date_button);
-        dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.US);
-        chooseDateButton.setText(dateFormat.format(scheduleDateTime.getTime()));
         chooseDateButton.setOnClickListener(this);
+        updateDateButton();
 
         // init time
         chooseTimeButton = (Button)findViewById(R.id.choose_time_button);
         timeFormat = new SimpleDateFormat("HH:mm", Locale.US);
-        chooseTimeButton.setText(timeFormat.format(scheduleDateTime.getTime()));
+        updateTimeButton(scheduleDateTime);
         chooseTimeButton.setOnClickListener(this);
     }
 
@@ -75,11 +72,12 @@ public class SchedulePostDialog extends Dialog implements View.OnClickListener, 
                 .show();
                 return;
             case R.id.choose_date_button:
-                new DatePickerDialog(getContext(), this,
+                final DatePickerDialog dialog = new DatePickerDialog(getContext(), this,
                         scheduleDateTime.get(Calendar.YEAR),
                         scheduleDateTime.get(Calendar.MONTH),
-                        scheduleDateTime.get(Calendar.DAY_OF_MONTH))
-                .show();
+                        scheduleDateTime.get(Calendar.DAY_OF_MONTH));
+                dialog.getDatePicker().setMinDate(System.currentTimeMillis());
+                dialog.show();
         }
     }
 
@@ -87,11 +85,7 @@ public class SchedulePostDialog extends Dialog implements View.OnClickListener, 
         if (scheduleDateTime.getTimeInMillis() <= System.currentTimeMillis()) {
             new AlertDialog.Builder(getContext())
             .setMessage(R.string.scheduled_time_is_in_the_past_continue_title)
-            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int whichButton) {
-                    schedulePost();
-                }
-            })
+            .setPositiveButton(android.R.string.yes, (dialog, whichButton) -> schedulePost())
             .setNegativeButton(android.R.string.no, null)
             .show();
         } else {
@@ -102,21 +96,11 @@ public class SchedulePostDialog extends Dialog implements View.OnClickListener, 
     private void schedulePost() {
         findViewById(R.id.schedule_button).setEnabled(false);
         final Completable completable = Completable
-                .fromAction(new Action() {
-                    @Override
-                    public void run() throws Exception {
-                        Tumblr.getSharedTumblr(getContext())
-                                .schedulePost(blogName, item, scheduleDateTime.getTimeInMillis());
-                    }
-                })
+                .fromAction(() -> Tumblr.getSharedTumblr(getContext())
+                        .schedulePost(blogName, item, scheduleDateTime.getTimeInMillis()))
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .doFinally(new Action() {
-                    @Override
-                    public void run() throws Exception {
-                        dismiss();
-                    }
-                });
+                .doFinally(() -> dismiss());
         if (onPostSchedule != null) {
             onPostSchedule.onPostScheduled(item.getPostId(), scheduleDateTime, completable);
         }
@@ -125,7 +109,7 @@ public class SchedulePostDialog extends Dialog implements View.OnClickListener, 
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
         scheduleDateTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
         scheduleDateTime.set(Calendar.MINUTE, minute);
-        chooseTimeButton.setText(timeFormat.format(scheduleDateTime.getTime()));
+        updateTimeButton(scheduleDateTime);
     }
 
     @Override
@@ -133,11 +117,25 @@ public class SchedulePostDialog extends Dialog implements View.OnClickListener, 
         scheduleDateTime.set(Calendar.YEAR, year);
         scheduleDateTime.set(Calendar.MONTH, monthOfYear);
         scheduleDateTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-        chooseDateButton.setText(dateFormat.format(scheduleDateTime.getTime()));
+        updateDateButton();
     }
     
     public interface onPostScheduleListener {
         @SuppressWarnings("UnusedParameters")
         void onPostScheduled(long id, Calendar scheduledDateTime, Completable completable);
+    }
+
+    private void updateDateButton() {
+        CharSequence timeString = DateUtils.getRelativeTimeSpanString(
+                scheduleDateTime.getTimeInMillis(),
+                System.currentTimeMillis(),
+                DateUtils.DAY_IN_MILLIS,
+                DateUtils.FORMAT_SHOW_DATE);
+
+        chooseDateButton.setText(timeString);
+    }
+
+    private void updateTimeButton(Calendar scheduleDateTime) {
+        chooseTimeButton.setText(timeFormat.format(scheduleDateTime.getTime()));
     }
 }
