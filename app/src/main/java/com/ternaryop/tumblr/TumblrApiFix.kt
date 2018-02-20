b02cebc617b20ea7b33ae1ec13026deedd894996
Davide Ficano <davide.ficano@gmail.com>
2018-02-20 06:28:38 +0100

    moved constants to companion object

--- app/src/main/java/com/ternaryop/tumblr/TumblrApiFix.kt
+++ app/src/main/java/com/ternaryop/tumblr/TumblrApiFix.kt
@@ -8,10 +8,6 @@ import org.scribe.model.Token
  * Tumblr suddenly switched to HTTPS for token resource urls so this class overrides the original one
  */
 class TumblrApiFix : TumblrApi() {
-    private val AUTHORIZE_URL = "https://www.tumblr.com/oauth/authorize?oauth_token=%s"
-    private val REQUEST_TOKEN_RESOURCE = "https://www.tumblr.com/oauth/request_token"
-    private val ACCESS_TOKEN_RESOURCE = "https://www.tumblr.com/oauth/access_token"
-
     override fun getAccessTokenEndpoint(): String {
         return ACCESS_TOKEN_RESOURCE
     }
@@ -22,5 +18,11 @@ class TumblrApiFix : TumblrApi() {
 
     override fun getAuthorizationUrl(requestToken: Token): String {
         return String.format(AUTHORIZE_URL, requestToken.token)
+    }
+
+    companion object {
+        private const val AUTHORIZE_URL = "https://www.tumblr.com/oauth/authorize?oauth_token=%s"
+        private const val REQUEST_TOKEN_RESOURCE = "https://www.tumblr.com/oauth/request_token"
+        private const val ACCESS_TOKEN_RESOURCE = "https://www.tumblr.com/oauth/access_token"
     }
 }
