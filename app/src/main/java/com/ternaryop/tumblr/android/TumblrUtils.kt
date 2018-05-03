6c2ca35692a6d0dc29e1d760dbea569091dff46a
Davide Ficano <davide.ficano@gmail.com>
2018-05-03 15:41:08 +0200

    refactored to extension added editTags

--- app/src/main/java/com/ternaryop/tumblr/android/TumblrUtils.kt
+++ app/src/main/java/com/ternaryop/tumblr/android/TumblrUtils.kt
@@ -1,62 +1,78 @@
 package com.ternaryop.tumblr.android
 
-import android.content.Context
-import com.ternaryop.photoshelf.db.DBHelper
+import com.ternaryop.tumblr.Tumblr
 import com.ternaryop.tumblr.TumblrPost
 
-object TumblrUtils {
-    fun renameTag(fromTag: String, toTag: String, context: Context, blogName: String): Int {
-        val searchParams = HashMap<String, String>()
-        searchParams["type"] = "photo"
-        searchParams["tag"] = fromTag
-        var offset = 0
-        var loadNext: Boolean
-        val tumblr = TumblrManager.getInstance(context)
-        val params = HashMap<String, String>()
-        var renamedCount = 0
+fun Tumblr.editTags(postId: Long, blogName: String, tagsToAdd: List<String>): List<String>? {
+    val postsList = getPublicPosts(blogName, mapOf("id" to postId.toString()))
 
-        do {
-            searchParams["offset"] = offset.toString()
-            val postsList = tumblr.getPublicPosts(blogName, searchParams)
-            loadNext = postsList.isNotEmpty()
-            offset += postsList.size
-
-            for (post in postsList) {
-                if (replaceTag(fromTag, toTag, post)) {
-                    params.clear()
-                    params["id"] = post.postId.toString()
-                    params["tags"] = post.tagsAsString
-                    tumblr.editPost(blogName, params)
-                    updateTagsOnDB(context, post.postId, post.tagsAsString, blogName)
-                    ++renamedCount
-                }
-            }
-        } while (loadNext)
-        return renamedCount
+    if (postsList.isEmpty()) {
+        return null
     }
 
-    private fun replaceTag(fromTag: String, toTag: String, post: TumblrPost): Boolean {
-        val renamedTag = ArrayList(post.tags)
-        var found = false
-        for (i in renamedTag.indices) {
-            if (renamedTag[i].equals(fromTag, ignoreCase = true)) {
-                renamedTag.removeAt(i)
-                renamedTag.add(i, toTag)
-                found = true
-                break
+    val tagList = TumblrPost.tagsFromString(postsList[0].tagsAsString)
+    val currentCount = tagList.size
+    for (t in tagsToAdd) {
+        if (!tagList.contains(t)) {
+            tagList.add(t)
+        }
+    }
+    // nothing added
+    if (currentCount == tagList.size) {
+        return null
+    }
+
+    val params = mapOf(
+    "id" to postId.toString(),
+    "tags" to tagList.joinToString(",")
+    )
+    editPost(blogName, params)
+    return tagList
+}
+
+fun Tumblr.renameTag(fromTag: String, toTag: String, blogName: String, onEdited: (post: TumblrPost) -> Unit): Int {
+    val searchParams = HashMap<String, String>()
+    searchParams["type"] = "photo"
+    searchParams["tag"] = fromTag
+    var offset = 0
+    var loadNext: Boolean
+    val params = HashMap<String, String>()
+    var renamedCount = 0
+
+    do {
+        searchParams["offset"] = offset.toString()
+        val postsList = getPublicPosts(blogName, searchParams)
+        loadNext = postsList.isNotEmpty()
+        offset += postsList.size
+
+        for (post in postsList) {
+            if (replaceTag(fromTag, toTag, post)) {
+                params.clear()
+                params["id"] = post.postId.toString()
+                params["tags"] = post.tagsAsString
+                editPost(blogName, params)
+                onEdited(post)
+//                DBHelper.getInstance(context).postDAO.updateTags(context, post.postId, post.tagsAsString, blogName)
+                ++renamedCount
             }
         }
-        if (found) {
-            post.tags = renamedTag
-        }
-        return found
-    }
+    } while (loadNext)
+    return renamedCount
+}
 
-    private fun updateTagsOnDB(context: Context, id: Long, tags: String, blogName: String) {
-        val newValues = HashMap<String, String>()
-        newValues["id"] = id.toString()
-        newValues["tags"] = tags
-        newValues["tumblrName"] = blogName
-        DBHelper.getInstance(context).postDAO.update(context, newValues)
+private fun replaceTag(fromTag: String, toTag: String, post: TumblrPost): Boolean {
+    val renamedTag = ArrayList(post.tags)
+    var found = false
+    for (i in renamedTag.indices) {
+        if (renamedTag[i].equals(fromTag, ignoreCase = true)) {
+            renamedTag.removeAt(i)
+            renamedTag.add(i, toTag)
+            found = true
+            break
+        }
     }
+    if (found) {
+        post.tags = renamedTag
+    }
+    return found
 }
