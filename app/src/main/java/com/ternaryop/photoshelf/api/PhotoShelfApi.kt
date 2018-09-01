a8e5365657c275e061abc80977ce0e0926a55e0c
Davide Ficano <davide.ficano@gmail.com>
2018-09-01 11:20:55 +0200

    increased the read timeout

--- app/src/main/java/com/ternaryop/photoshelf/api/PhotoShelfApi.kt
+++ app/src/main/java/com/ternaryop/photoshelf/api/PhotoShelfApi.kt
@@ -10,6 +10,7 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.Calendar
+import java.util.concurrent.TimeUnit
 
 class Response<T>(val response: T)
 
@@ -30,6 +31,7 @@ class PhotoShelfApi(private val accessToken: String) {
 
         val builder = OkHttpClient.Builder()
         builder.interceptors().add(interceptor)
+        builder.readTimeout(5, TimeUnit.MINUTES)
 
         if (BuildConfig.DEBUG) {
             val debugInterceptor : HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
