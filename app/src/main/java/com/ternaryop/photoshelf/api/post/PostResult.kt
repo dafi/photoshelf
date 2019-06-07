d664d5fd4c7fbb923ac4fc43cf9baf712bada18f
Davide Ficano <davide.ficano@gmail.com>
2019-06-07 16:52:22 +0200

    fixed compile errors after OkHttp upgrade (again!!)

--- app/src/main/java/com/ternaryop/photoshelf/api/post/PostResult.kt
+++ app/src/main/java/com/ternaryop/photoshelf/api/post/PostResult.kt
@@ -1,6 +1,6 @@
 package com.ternaryop.photoshelf.api.post
 
-import okhttp3.MediaType.Companion.get
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 
@@ -29,4 +29,4 @@ data class TagInfo(val tag: String, var postCount: Long) {
 data class TagInfoListResult(val tags: List<TagInfo>)
 
 fun titlesRequestBody(titles: Collection<String>): RequestBody =
-    titles.joinToString("\n").toRequestBody(get("text/plain"))
+    titles.joinToString("\n").toRequestBody("text/plain".toMediaTypeOrNull())
