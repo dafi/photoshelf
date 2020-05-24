883eee607b91a9683276e9d918c11da36338f785
Davide Ficano <davide.ficano@gmail.com>
2020-05-24 16:16:52 +0200

    fixed Coil to use builders instead of deprecated DSL

--- app/src/main/java/com/ternaryop/photoshelf/util/image/ImageLoader.kt
+++ app/src/main/java/com/ternaryop/photoshelf/util/image/ImageLoader.kt
@@ -7,10 +7,9 @@ import com.ternaryop.photoshelf.R
 
 object ImageLoader {
     fun setup(context: Context) {
-        Coil.setDefaultImageLoader {
-            ImageLoader(context) {
-                error(R.drawable.ic_sync_problem_black_24dp)
-            }
-        }
+        Coil.setImageLoader(
+            ImageLoader.Builder(context)
+                .error(R.drawable.ic_sync_problem_black_24dp)
+                .build())
     }
 }
